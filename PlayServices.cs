6e889bcc81e827566fc344f130318bc197d07ef0
Abd Majeed Abd Mumuni <38582706+BitsAndWits@users.noreply.github.com>
2020-08-22 23:07:07 +0100

    Add files via upload

---
+++ PlayServices.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+
+public class PlayServices : MonoBehaviour
+{
+    public int playerScore;
+    string leaderboardID = "";
+    string achievementID = "";
+
+    void Start()
+    {
+
+        DontDestroyOnLoad(this);
+        try
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+            Social.localUser.Authenticate((bool success) => { });
+        }
+        catch (Exception exception)
+        {
+            Debug.Log(exception);
+        }
+    }
+
+    public void AddScoreToLeaderboard()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportScore(playerScore, leaderboardID, success => { });
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ShowLeaderboardUI();
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ShowAchievementsUI();
+        }
+    }
+
+    public void UnlockAchievement()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportProgress(achievementID, 100f, success => { });
+        }
+    }
+}
